cd5eb9f51ef728bd93fbb4da98d9d802c00f582e
Pankaj Meghwal <129221700+thePankaj021@users.noreply.github.com>
2026-06-04 14:30:04 +0530

    Time: 957 ms (26.38%), Space: 42 MB (71.78%) - LeetHub

---
+++ 0730-count-different-palindromic-subsequences/0730-count-different-palindromic-subsequences.py
@@ -0,0 +1,42 @@
+class Solution:
+    def countPalindromicSubsequences(self, s: str) -> int:
+        MOD = 10**9 + 7
+        n = len(s)
+
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = 1
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                if s[i] == s[j]:
+                    low, high = i + 1, j - 1
+
+                    while low <= high and s[low] != s[i]:
+                        low += 1
+
+                    while low <= high and s[high] != s[i]:
+                        high -= 1
+
+                    if low > high:
+                        dp[i][j] = dp[i + 1][j - 1] * 2 + 2
+                    elif low == high:
+                        dp[i][j] = dp[i + 1][j - 1] * 2 + 1
+                    else:
+                        dp[i][j] = (
+                            dp[i + 1][j - 1] * 2
+                            - dp[low + 1][high - 1]
+                        )
+                else:
+                    dp[i][j] = (
+                        dp[i + 1][j]
+                        + dp[i][j - 1]
+                        - dp[i + 1][j - 1]
+                    )
+
+                dp[i][j] %= MOD
+
+        return dp[0][n - 1]
