560a4c4f9bbf92defdecbd709ed59631736da6da
Pankaj Meghwal <129221700+thePankaj021@users.noreply.github.com>
2026-06-09 11:21:42 +0530

    Time: 11 ms (82.2%), Space: 20.6 MB (64.07%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        if not timeSeries:
+            return 0
+
+        total = 0
+
+        for i in range(len(timeSeries) - 1):
+            total += min(duration, timeSeries[i + 1] - timeSeries[i])
+
+        return total + duration
